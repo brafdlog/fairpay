160d4beff3f79d90d65c8e9d8b8650531945c954
brafdlog <brafdlog@gmail.com>
2015-04-23 23:06:45 +0300

    Add color and animation to our clients section and replace windows line ending with unix

--- wp-content/themes/zerif-lite/sections/our_clients.php
+++ wp-content/themes/zerif-lite/sections/our_clients.php
@@ -1,1 +1,34 @@
-<section class="ourClients" id="ourClients"><div class="container">  <div class="row">        <div class="col-md-4 col-sm-4 col-xs-12 clientsItem">  		<div class="clientsItemInnerWrapper">  			<i class="fa fa-copyright"></i>        <h3>Employers</h3>        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>  		</div>    </div>        <div class="col-md-4 col-sm-4 col-xs-12 clientsItem">      <div class="clientsItemInnerWrapper">        <i class="fa fa-user"></i>        <h3>Independant Workers</h3>        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>      </div>    </div>        <div class="col-md-4 col-sm-4 col-xs-12 clientsItem">      <div class="clientsItemInnerWrapper">        <i class="fa fa-at"></i>        <h3>Freelance Websites</h3>        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>      </div>    </div>  </div></div> <!-- / END CONTAINER --></section>  <!-- / END our clients section -->
+<section class="ourClients" id="ourClients">
+
+<div class="container">
+
+  <div class="row">
+    
+    <div class="col-md-4 col-sm-4 col-xs-12 clientsItem" data-scrollreveal="enter right after 0s over 1s">
+  		<div class="clientsItemInnerWrapper">
+  			<i class="fa fa-copyright"></i>
+        <h3>Employers</h3>
+        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>
+  		</div>
+    </div>
+    
+    <div class="col-md-4 col-sm-4 col-xs-12 clientsItem" data-scrollreveal="enter right after 0s over 1s">
+      <div class="clientsItemInnerWrapper">
+        <i class="fa fa-user"></i>
+        <h3>Independant Workers</h3>
+        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>
+      </div>
+    </div>
+    
+    <div class="col-md-4 col-sm-4 col-xs-12 clientsItem" data-scrollreveal="enter right after 0s over 1s">
+      <div class="clientsItemInnerWrapper">
+        <i class="fa fa-at"></i>
+        <h3>Freelance Websites</h3>
+        <p>Lorem ipsum et dolor amet<br> consectetuer diam</p>
+      </div>
+    </div>
+
+  </div>
+
+</div> <!-- / END CONTAINER -->
+</section>  <!-- / END our clients section -->
