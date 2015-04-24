e5cf096463e83366baf4a7d37935346397816bdf
brafdlog <brafdlog@gmail.com>
2015-04-24 17:54:48 +0300

    Add a fourth column to our clients

--- wp-content/themes/zerif-lite/sections/our_clients.php
+++ wp-content/themes/zerif-lite/sections/our_clients.php
@@ -22,14 +22,22 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-user fa-3x"></i>
         <h3>Independent Workers</h3>
-        <p>Paying too much for healthcare?<br>Tired of changing healthcare <br> provider every time you <br>get a new job? <br><br> Sign up today for the employee benefit<br> package that goes with you.Get<br>  better services employers <br>can contribute to.<br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p>Paying too much for healthcare?<br>Tired of changing healthcare <br> provider every time you <br>get a new job? <br><br> Sign up today for the employee benefit<br> package that goes with you. Get<br>  better services employers <br>can contribute to.<br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
     </div>
     
     <div class="col-md-4 col-sm-4 col-xs-12 clientsItem" data-scrollreveal="enter right after 0s over 1s">
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-suitcase fa-3x"></i>
-        <h3>Employers</h3>
+        <h3>Companies</h3>
+        <p>Not sure how to give your contractors<br> and freelancers employee benefits? <br> Want to make sure your house<br>  cleaner has healthcare?<br><br>Contact us today to learn how<br> you can give everyone who <br>works for you the benefits<br> they deserve. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+      </div>
+    </div>
+
+    <div class="col-md-4 col-sm-4 col-xs-12 clientsItem" data-scrollreveal="enter right after 0s over 1s">
+      <div class="clientsItemInnerWrapper">
+        <i class="fa fa-home fa-3x"></i>
+        <h3>Private Employers</h3>
         <p>Not sure how to give your contractors<br> and freelancers employee benefits? <br> Want to make sure your house<br>  cleaner has healthcare?<br><br>Contact us today to learn how<br> you can give everyone who <br>works for you the benefits<br> they deserve. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
     </div>
