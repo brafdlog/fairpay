1f8eea6f98116a7bb19f30bb3a29d492621e0995
brafdlog <brafdlog@gmail.com>
2015-04-24 17:26:33 +0300

    Change icons in our_clients

--- wp-content/themes/zerif-lite/sections/our_clients.php
+++ wp-content/themes/zerif-lite/sections/our_clients.php
@@ -12,7 +12,7 @@
     
     <div class="col-md-4 col-sm-4 col-xs-12 clientsItem clientsItemEmployers" data-scrollreveal="enter right after 0s over 1s">
   		<div class="clientsItemInnerWrapper">
-  			<i class="fa fa-copyright"></i>
+  			<i class="fa fa-globe fa-3x"></i>
         <h3>On-Demand Economy Apps</h3>
         <p>Why not give your consumers  <br> the option of paying for  <br>employee benefits?  <br> <br>Partner with us today   to enable your <br>consumers to give service  providers <br>access to better healthcare, <br> a retirement plan, and <br>paid holidays.<br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
   		</div>
@@ -20,7 +20,7 @@
     
     <div class="col-md-4 col-sm-4 col-xs-12 clientsItem clientsItemIndependantWorkers" data-scrollreveal="enter right after 0s over 1s">
       <div class="clientsItemInnerWrapper">
-        <i class="fa fa-user"></i>
+        <i class="fa fa-user fa-3x"></i>
         <h3>Independent Workers</h3>
         <p>Paying too much for healthcare?<br>Tired of changing healthcare <br> provider every time you <br>get a new job? <br><br> Sign up today for the employee benefit<br> package that goes with you.Get<br>  better services employers <br>can contribute to.<br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
@@ -28,7 +28,7 @@
     
     <div class="col-md-4 col-sm-4 col-xs-12 clientsItem clientsItemFreelance" data-scrollreveal="enter right after 0s over 1s">
       <div class="clientsItemInnerWrapper">
-        <i class="fa fa-at"></i>
+        <i class="fa fa-suitcase fa-3x"></i>
         <h3>Employers</h3>
         <p>Not sure how to give your contractors<br> and freelancers employee benefits? <br> Want to make sure your house<br>  cleaner has healthcare?<br><br>Contact us today to learn how<br> you can give everyone who <br>works for you the benefits<br> they deserve. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
