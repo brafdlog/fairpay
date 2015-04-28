39cbd255792108e780e13d33027e0fa226747579
brafdlog <brafdlog@gmail.com>
2015-04-28 13:12:50 -0700

    Avi's changes to our_clients text

--- wp-content/themes/zerif-lite/sections/our_clients.php
+++ wp-content/themes/zerif-lite/sections/our_clients.php
@@ -14,7 +14,8 @@
   		<div class="clientsItemInnerWrapper">
   			<i class="fa fa-globe fa-3x"></i>
         <h3>On-Demand Economy Apps</h3>
-        <p>Why not give your consumers  <br> the option of paying for  <br>employee benefits?  <br> <br>Partner with us today   to enable your <br>consumers to give service  providers <br>access to better healthcare, <br> a retirement plan, and <br>paid holidays.<br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+     <p><b>Who:</b>  Freelance websites, ride-sharing apps, home-services apps, etc. <br><br> <b>The Problem: </b>Service providers working through your on-demand economy apps do not receive any employee benefits. This emerging class of workers doesn't receive any of the benefits their full-time employee counterparts do.<br> <br>
+<b> How we help: </b>Partner with us today to enable your consumers to contribute towards the service provider's healthcare, retirement plan and holidays. Consumers can choose to opt-in, and we'll manage the benefits!<br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service &gt;&gt;</a></p>
   		</div>
     </div>
     
@@ -22,15 +23,15 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-user fa-3x"></i>
         <h3>Independent Workers</h3>
-        <p>Paying too much for healthcare?<br>Tired of changing healthcare <br> provider every time you <br>get a new job? <br><br> Sign up today for the employee benefit<br> package that goes with you. Get<br>  better services employers <br>can contribute to.<br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p><b>Who:</b> Freelancers of all types- photographers, web-designers, plumbers and anyone else who receives a 1099 at the end of the year.<br><br> <b>The Problem: </b> Your clients have no way of contributing directly towards your employee benefits. Why not give your clients the option of paying for your healthcare? <br><br><b> How we help: </b> Sign up today for the employee benefit package that goes with you! We'll manage your benefits for you, and your clients can contribute directly towards your benefits whether you worked for them for an hour or a year. <br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
     </div>
     
     <div class="col-md-3 col-sm-3 col-xs-12 clientsItem">
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-suitcase fa-3x"></i>
-        <h3>Companies</h3>
-        <p>Not sure how to give your contractors<br> and freelancers employee benefits? <br> Want to make sure your house<br>  cleaner has healthcare?<br><br>Contact us today to learn how<br> you can give everyone who <br>works for you the benefits<br> they deserve. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <h3>Businesses</h3>
+        <p><b>Who:</b>Companies that hire contractors or temporary workers. In today's economy, that means most companies.<br><br> <b>The Problem: </b> Not sure how to give your freelancers their employee benefits? Want to do the right thing but not sure how you give someone healthcare if they are only with you for a couple of months?  <br><br><b> How we help: </b> Sign up with us today to give anyone working for you their employee benefits. All you have to do is tell us how many hours they worked for you, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
     </div>
 
@@ -38,7 +39,7 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-home fa-3x"></i>
         <h3>Private Employers</h3>
-        <p>Not sure how to give your contractors<br> and freelancers employee benefits? <br> Want to make sure your house<br>  cleaner has healthcare?<br><br>Contact us today to learn how<br> you can give everyone who <br>works for you the benefits<br> they deserve. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p><b>Who:</b> Individuals who hire home cleaners, gardeners, nannies, etc. <br><br> <b>The Problem: </b> Do you know if your house cleaner has healthcare or a retirement plan? Want to give your nanny her employee benefits?  <br><br><b> How we help:</b> Sign up with us today to make sure that anyone working for you is getting what they deserve. Tell us what benefits you want to give them, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
       </div>
     </div>
 
