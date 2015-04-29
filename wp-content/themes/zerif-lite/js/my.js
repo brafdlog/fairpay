ff0a5713e81be46eb8cc03ebcf9f16807dcd9efe
brafdlog <brafdlog@gmail.com>
2015-04-30 01:04:37 +0300

    Add an arrow to our client items

--- wp-content/themes/zerif-lite/js/my.js
+++ wp-content/themes/zerif-lite/js/my.js
@@ -1,14 +1,17 @@
 jQuery(document).ready(function() {
 	jQuery('.clientsItem').off('click').on('click', function() {
-		var paragraphOfClickedItem = jQuery(this).find('p');
+		var clickedItem = jQuery(this);
+		var paragraphOfClickedItem = clickedItem.find('p');
 		
 		var alreadyOpen = paragraphOfClickedItem.is(":visible");
 		
 		// Fold visible paragraph so we don't have a state of two visible paragraphs at the same time
 		jQuery('#ourClientsContentRow p:visible').slideUp();
+		clickedItem.find('i.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
 		
 		if (!alreadyOpen) {
 			paragraphOfClickedItem.slideDown();
+			clickedItem.find('i.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
 		}
 	});
 });
