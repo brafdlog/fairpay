0be3479c0b54389f7f3d63282b62f5c9dca441ca
brafdlog <brafdlog@gmail.com>
2015-04-30 01:33:17 +0300

    Link read more from our clients to subscribe form

--- wp-content/themes/zerif-lite/sections/our_clients.php
+++ wp-content/themes/zerif-lite/sections/our_clients.php
@@ -15,7 +15,7 @@
   			<i class="fa fa-globe fa-3x"></i>
         <h3>On-Demand Economy Apps</h3>
         <p><b>Who:</b>  Freelance websites, ride-sharing apps, home-services apps, etc. <br><br> <b>The Problem: </b>Service providers working through your on-demand economy apps do not receive any employee benefits. This emerging class of workers doesn't receive any of the benefits their full-time employee counterparts do.<br> <br>
-        <b> How we help: </b>Partner with us today to enable your consumers to contribute towards the service provider's healthcare, retirement plan and holidays. Consumers can choose to opt-in, and we'll manage the benefits!<br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service &gt;&gt;</a></p>
+        <b> How we help: </b>Partner with us today to enable your consumers to contribute towards the service provider's healthcare, retirement plan and holidays. Consumers can choose to opt-in, and we'll manage the benefits!<br><br>Coming Soon! <br><a href="/subscribe" class="readMoreLink">Be the first to know when we launch this service &gt;&gt;</a></p>
         <i class="fa fa-chevron-down"></i>
   		</div>
     </div>
@@ -24,7 +24,7 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-user fa-3x"></i>
         <h3>Independent Workers</h3>
-        <p><b>Who:</b> Freelancers of all types- photographers, web-designers, plumbers and anyone else who receives a 1099 at the end of the year.<br><br> <b>The Problem: </b> Your clients have no way of contributing directly towards your employee benefits. Why not give your clients the option of paying for your healthcare? <br><br><b> How we help: </b> Sign up today for the employee benefit package that goes with you! We'll manage your benefits for you, and your clients can contribute directly towards your benefits whether you worked for them for an hour or a year. <br><br> Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p><b>Who:</b> Freelancers of all types- photographers, web-designers, plumbers and anyone else who receives a 1099 at the end of the year.<br><br> <b>The Problem: </b> Your clients have no way of contributing directly towards your employee benefits. Why not give your clients the option of paying for your healthcare? <br><br><b> How we help: </b> Sign up today for the employee benefit package that goes with you! We'll manage your benefits for you, and your clients can contribute directly towards your benefits whether you worked for them for an hour or a year. <br><br> Coming Soon! <br><a href="/subscribe" class="readMoreLink">Be the first to know when we launch this service >></a></p>
         <i class="fa fa-chevron-down"></i>
       </div>
     </div>
@@ -33,7 +33,7 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-suitcase fa-3x"></i>
         <h3>Businesses</h3>
-        <p><b>Who:</b> Companies that hire contractors or temporary workers. In today's economy, that means most companies.<br><br> <b>The Problem: </b> Not sure how to give your freelancers their employee benefits? Want to do the right thing but not sure how you give someone healthcare if they are only with you for a couple of months?  <br><br><b> How we help: </b> Sign up with us today to give anyone working for you their employee benefits. All you have to do is tell us how many hours they worked for you, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p><b>Who:</b> Companies that hire contractors or temporary workers. In today's economy, that means most companies.<br><br> <b>The Problem: </b> Not sure how to give your freelancers their employee benefits? Want to do the right thing but not sure how you give someone healthcare if they are only with you for a couple of months?  <br><br><b> How we help: </b> Sign up with us today to give anyone working for you their employee benefits. All you have to do is tell us how many hours they worked for you, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/subscribe" class="readMoreLink">Be the first to know when we launch this service >></a></p>
         <i class="fa fa-chevron-down"></i>
       </div>
     </div>
@@ -42,7 +42,7 @@
       <div class="clientsItemInnerWrapper">
         <i class="fa fa-home fa-3x"></i>
         <h3>Private Employers</h3>
-        <p><b>Who:</b> Individuals who hire home cleaners, gardeners, nannies, etc. <br><br> <b>The Problem: </b> Do you know if your house cleaner has healthcare or a retirement plan? Want to give your nanny her employee benefits?  <br><br><b> How we help:</b> Sign up with us today to make sure that anyone working for you is getting what they deserve. Tell us what benefits you want to give them, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/independent-workers" class="readMoreLink">Be the first to know when we launch this service >></a></p>
+        <p><b>Who:</b> Individuals who hire home cleaners, gardeners, nannies, etc. <br><br> <b>The Problem: </b> Do you know if your house cleaner has healthcare or a retirement plan? Want to give your nanny her employee benefits?  <br><br><b> How we help:</b> Sign up with us today to make sure that anyone working for you is getting what they deserve. Tell us what benefits you want to give them, and we'll handle the rest. <br><br>Coming Soon! <br><a href="/subscribe" class="readMoreLink">Be the first to know when we launch this service >></a></p>
         <i class="fa fa-chevron-down"></i>
       </div>
     </div>
